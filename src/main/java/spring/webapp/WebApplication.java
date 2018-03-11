4603d15b40834d7645c10fbd5f9ec1a14a073f81
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2018-03-11 16:31:35 +0700

    Add .gitignore, clean-up pom file, fixed controller-view (with Spring)

--- src/main/java/spring/webapp/WebApplication.java
+++ src/main/java/spring/webapp/WebApplication.java
@@ -1,21 +1,20 @@
+package spring.webapp;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @SpringBootApplication
 @Configuration
-@EnableScheduling
-@EnableCaching
 public class WebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class,args);
+        SpringApplication.run(WebApplication.class, args);
     }
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
