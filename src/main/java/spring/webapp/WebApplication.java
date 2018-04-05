f7471feacbd8b5a578420e933a30a5ea7a7295bc
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2018-04-05 23:13:28 +0700

    Will now create a deployable .war file.

--- src/main/java/spring/webapp/WebApplication.java
+++ src/main/java/spring/webapp/WebApplication.java
@@ -2,6 +2,8 @@ package spring.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
@@ -10,7 +12,13 @@ import org.springframework.web.servlet.view.JstlView;
 
 @SpringBootApplication
 @Configuration
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
